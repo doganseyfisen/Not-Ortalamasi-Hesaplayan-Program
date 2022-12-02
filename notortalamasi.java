d2662e39ef340114ada49100a7c7b128f59c9e12
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-02 17:49:46 +0300

    Create notortalamasi.java

---
+++ notortalamasi.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Driver {
+	public static void main(String[] args) {
+		
+		int matematik, fizik, kimya, turkce, tarih, muzik;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Matematik sınav notunuz: ");
+		matematik = scanner.nextInt();
+		
+		System.out.print("Fizik sınav notunuz: ");
+		fizik = scanner.nextInt();
+		
+		System.out.print("Kimya sınav notunuz: ");
+		kimya = scanner.nextInt();
+		
+		System.out.print("Türkçe sınav notunuz: ");
+		turkce = scanner.nextInt();
+		
+		System.out.print("Tarih sınav notunuz: ");
+		tarih = scanner.nextInt();
+		
+		System.out.print("Müzik sınav notunuz: ");
+		muzik = scanner.nextInt();
+		
+		int toplam = (matematik + fizik + kimya + turkce + tarih + muzik);
+		double ortalama = toplam/7.0;
+		
+		System.out.println("Genel ortalama: " + ortalama);
+		
+		System.out.println(ortalama > 60 ? "Sınıfı Geçti." : "Sınıfta kaldı.");
+	}
+}
